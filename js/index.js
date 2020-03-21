5d196de94eaa497ac82288dae02bb64b6238e118
kuma-9106 <61691226+kuma-9106@users.noreply.github.com>
2020-03-21 09:59:17 +0900

    error箇所修正

--- js/index.js
+++ js/index.js
@@ -61,7 +61,10 @@ window.onload = function(){
             }
 
      function animationReset() {
-              charts.style.backgroundPositionY = '0px';
+	    　charts.forEach(chart => {
+	      chart.style.backgroundPositionY = '0px';
+	     });
+        
 			        /*console.log(charts.length);*/
               isDoing = false;
             }
